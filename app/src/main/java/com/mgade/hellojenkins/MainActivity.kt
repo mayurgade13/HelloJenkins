879f2b00f542efde27163c69c2a79769ec29ed22
Mayur Gade <mgade@Mayurs-Mac-mini.local>
2025-09-13 20:02:30 +0530

    initial-commit

---
+++ app/src/main/java/com/mgade/hellojenkins/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mgade.hellojenkins
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mgade.helloJenkins.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
